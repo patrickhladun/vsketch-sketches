64637ec995fa358c48affe4ab37b2dbcb1bacc06
Patrick Hladun <patrickhladun@gmail.com>
2023-03-10 19:07:52 +0000

    Add size, paper and layers params

--- sketches/10-print/sketch-10-print.py
+++ sketches/10-print/sketch-10-print.py
@@ -1,13 +1,13 @@
 import vsketch
 
-class TenPrintSketch(vsketch.SketchClass):
-    # Sketch parameters:
-    size = vsketch.Param(6)
+class TenCodeSketch(vsketch.SketchClass):
+    size = vsketch.Param(4)
     paper = vsketch.Param('a4', choices=[
         'a1', 'a2', 'a3', 'a4', 'a5', 'a6'
     ])
-    num_x = vsketch.Param(30)
-    num_y = vsketch.Param(45)
+    layers = vsketch.Param(1, 1, 12)
+    cols = vsketch.Param(40)
+    rows = vsketch.Param(60)
     probability = vsketch.Param(0.60)
 
     def draw(self, vsk: vsketch.Vsketch) -> None:
@@ -16,16 +16,16 @@ class TenPrintSketch(vsketch.SketchClass):
         x = 0
         y = 0
 
-        for j in range(self.num_y):
+        for j in range(self.rows):
             x = 0
             y = y + self.size
-            for i in range(self.num_x):
+            for i in range(self.cols):
+                vsk.stroke(int(vsk.random(1, self.layers + 1)))
                 if(vsk.random(1) < self.probability):
                     vsk.line(x, y, x + self.size, y + self.size)
                 else:
                     vsk.line(x + self.size, y, x, y + self.size)
                 x = x + self.size
-
 
     def finalize(self, vsk: vsketch.Vsketch) -> None:
         vsk.vpype("linemerge linesimplify reloop linesort")
